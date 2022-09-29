d6aa041ec724f6779bf8e2b5a4a2cd055514be63
charlesdzadu <dkccharlie@gmail.com>
2022-09-29 22:03:03 +0000

    Update project

--- medias/models.py
+++ medias/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Media(models.Model):
     """La classe répresentant les médias """
+    
     name = models.CharField(max_length=100, help_text="Pour décrire un text") 
--- medias/views.py
+++ medias/views.py
@@ -1,7 +1,11 @@
+import os
+import requests
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.http import StreamingHttpResponse
+
 
 
 from medias.models import Media
@@ -9,16 +13,22 @@ from medias.serializers import MediaSerializer
 
 
 
-class MediasViewSet(ModelViewSet):
+
+class MediasViewSet(ViewSet):
     serializer_class = MediaSerializer
+    
     def get_queryset(self):
         medias = Media.objects.all()
         return medias
 
-    @action(detail=False, methods=['post'])
-    def disable(self):
+    @action(detail=False , methods=['get'])
+    def disable(self, request):
         """ Cette fonction permet de désactiver les médias"""
-        return  Response()
+        return  Response(data = "Je ne sais pas")
+    
+   
+
+    
 
 
     
